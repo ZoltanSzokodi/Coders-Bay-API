f475883608b35b0786f1fbc960159e26decb2e20
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-03-26 17:00:49 +0100

    asyncHandler middleware

--- middleware/error.js
+++ middleware/error.js
@@ -1,9 +1,33 @@
-const errorHandler = (err, req, res, next) => {
-  console.log(err.stack.red);
+const ErrorResponse = require('../utils/errorResponse');
 
-  res.status(500).json({
+const errorHandler = (err, req, res, next) => {
+  let error = {...err};
+  error.message = err.message;
+
+  // for dev purposes
+  console.log(err);
+
+  // Mongoose bad object id
+  if (err.name === 'CastError') {
+    const message = `Resource not found with id ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = 'Duplicate field value';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose validaton errors
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
     success: false,
-    error: err.message
+    error: error.message || 'Server Error'
   });
 };
 
