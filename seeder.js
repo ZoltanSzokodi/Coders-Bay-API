c3dced0cc419827c01fb7d418906c41fa372606d
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-03-27 12:47:09 +0100

    Created a seeder.js file to import or destroy data in the DB

---
+++ seeder.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const colors = require('colors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({path: './config/config.env'});
+
+// Load models
+const Bootcamp = require('./model/Bootcamp');
+
+// Connect ro DB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+// Read JSON files
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+// Import data into DB
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log('Data imported...'.green.inverse);
+
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Delete data into DB
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log('Data deleted...'.red.inverse);
+
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// we need to run these commands in the console -> "node seeder -i" || "node seeder -d"
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
