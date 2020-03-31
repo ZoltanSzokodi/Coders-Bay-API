6dbfd58d3d774d2fe04844b90f3275d543efa20d
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-03-31 13:58:59 +0200

    User login

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 
 // @desc    Register user
-// @route   GET /api/v1/auth/register
+// @route   POST /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
   const {name, email, role, password} = req.body;
@@ -14,6 +14,39 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
     password
   });
+
+  // Create token using the methode defined on the model
+  const token = user.getSignedJwt();
+
+  res.status(200).json({
+    success: true,
+    token
+  });
+});
+// @desc    Login user
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const {name, email, password} = req.body;
+
+  // Validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please add your email and password', 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({email}).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Match entered password
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
 
   // Create token using the methode defined on the model
   const token = user.getSignedJwt();
