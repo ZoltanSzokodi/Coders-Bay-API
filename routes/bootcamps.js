98c5a39906d7a4beca33c98fba6cdb051d0adf07
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-03-31 15:16:21 +0200

    Auth Protect Middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -13,6 +13,9 @@ const {
 const Bootcamp = require('../models/Bootcamp');
 const advancedResults = require('../middleware/advancedResults');
 
+// Authorization
+const {protect} = require('../middleware/auth');
+
 // Include other resource routers
 const courseRouter = require('./courses');
 
@@ -21,17 +24,17 @@ router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
-router.route('/:id/photo').put(bootcampPhotoUpload);
+router.route('/:id/photo').put(protect, bootcampPhotoUpload);
 
 router
   .route('/')
   .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-  .post(postBootcamp);
+  .post(protect, postBootcamp);
 
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(updateBootcamp)
-  .delete(deleteBootcamp);
+  .put(protect, updateBootcamp)
+  .delete(protect, deleteBootcamp);
 
 module.exports = router;
--- routes/courses.js
+++ routes/courses.js
@@ -11,6 +11,9 @@ const {
 const Course = require('../models/Course');
 const advancedResults = require('../middleware/advancedResults');
 
+// Authorization
+const {protect} = require('../middleware/auth');
+
 router
   .route('/')
   .get(
@@ -20,12 +23,12 @@ router
     }),
     getCourses
   )
-  .post(addCourse);
+  .post(protect, addCourse);
 
 router
   .route('/:id')
   .get(getCourse)
-  .put(updateCourse)
-  .delete(deleteCourse);
+  .put(protect, updateCourse)
+  .delete(protect, deleteCourse);
 
 module.exports = router;
