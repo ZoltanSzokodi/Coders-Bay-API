911a908941250267961964e3e067fff3341a3858
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-04-03 14:51:52 +0200

    Get Single Review & Update Seeder

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
-const {getReviews} = require('../controllers/reviews');
+const {getReviews, getReview} = require('../controllers/reviews');
 
 const Review = require('../models/Review');
 
@@ -15,5 +15,7 @@ router.route('/').get(
   }),
   getReviews
 );
+
+router.route('/:id').get(getReview);
 
 module.exports = router;
--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({path: './config/config.env'});
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
 const User = require('./models/User');
+const Review = require('./models/Review');
 
 // Connect ro DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -29,12 +30,17 @@ const courses = JSON.parse(
 const users = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
 );
+
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8')
+);
 // Import data into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
+    await Review.create(reviews);
 
     console.log('Data imported...'.green.inverse);
 
@@ -50,6 +56,7 @@ const deleteData = async () => {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
     await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data deleted...'.red.inverse);
 
     process.exit();
